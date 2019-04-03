7b0d030c8b2544c6ccc259283d3a462ba443a270
Ankush Behl <cloudbehl@gmail.com>
2019-04-04 00:47:02 +0530

    Added functionality for Details, Inventory and health card

---
+++ src/components/StorageOverview/OCSHealth/Health.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import {
+  DashboardCard,
+  DashboardCardBody,
+  DashboardCardHeader,
+  DashboardCardTitle,
+} from '../../Dashboard/DashboardCard';
+import HealthBody from './HealthBody';
+// import TaskBody from './TaskBody';
+import { ClusterOverviewContextGenericConsumer } from '../../ClusterOverview/ClusterOverviewContext';
+import { InlineLoading } from '../../Loading';
+
+export const OCSHealth = ({ data, loaded }) => (
+  <DashboardCard>
+    <DashboardCardHeader>
+      <DashboardCardTitle>Health</DashboardCardTitle>
+    </DashboardCardHeader>
+    <DashboardCardBody className="ocs-ocshealth__row-status-item" isLoading={!loaded} LoadingComponent={InlineLoading}>
+      <HealthBody data={data} />
+    </DashboardCardBody>
+  </DashboardCard>
+);
+
+OCSHealth.defaultProps = {
+  loaded: false,
+};
+
+OCSHealth.propTypes = {
+  data: PropTypes.object.isRequired,
+  loaded: PropTypes.bool,
+};
+
+const OCSHealthConnected = () => (
+  <ClusterOverviewContextGenericConsumer Component={OCSHealth} dataPath="ocsHealthData" />
+);
+
+export default OCSHealthConnected;
